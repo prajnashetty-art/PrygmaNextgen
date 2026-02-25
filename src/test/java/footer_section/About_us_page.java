af2b3e3d378aa612099af44753d521fe3e9a0268
Prajna Shetty <Prajna Shetty@LAPTOP-9SLE8TDA>
2026-02-25 10:26:33 +0530

    About Us page

--- src/test/java/footer_section/About_us_page.java
+++ src/test/java/footer_section/About_us_page.java
@@ -17,7 +17,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+ 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
